411cbf42102a771cb8ebe3ae4a44a72c076db2b6
leoneil_li <nevermore_xd@sina.com>
2016-12-01 17:48:10 +0800

    dataoke: make sdb creation more reusable

--- convert_excel_to_sdb.py
+++ convert_excel_to_sdb.py
@@ -32,12 +32,21 @@ if os.path.exists('dataoke.sdb'):
     os.remove('dataoke.sdb')
 
 conn = sqlite3.connect('dataoke.sdb')
-sql_command = 'CREATE TABLE youpinhui (ID INTEGER PRIMARY KEY, 品类 TEXT, 商品名称 TEXT, 日常价 TEXT, 双12价 TEXT, 商品链接 TEXT, 推荐理由)'
-conn.execute(sql_command)
-conn.commit()
+# sql_command = 'CREATE TABLE youpinhui (ID INTEGER PRIMARY KEY, 品类 TEXT, 商品名称 TEXT, 日常价 TEXT,
+# 双12价 TEXT, 商品链接 TEXT, 推荐理由 TEXT)'
+# conn.execute(sql_command)
 
 for nrow in range(nrows):
     if nrow == 0:
+        sql_command = 'CREATE TABLE youpinhui (ID INTEGER PRIMARY KEY, \"%s\" TEXT, \"%s\" TEXT, \"%s\" TEXT, ' \
+                      '\"%s\" TEXT, \"%s\" TEXT, \"%s\"  TEXT)' % (sheet.cell_value(nrow, 0),
+                                                                   sheet.cell_value(nrow, 1),
+                                                                   sheet.cell_value(nrow, 2),
+                                                                   sheet.cell_value(nrow, 3),
+                                                                   sheet.cell_value(nrow, 4),
+                                                                   sheet.cell_value(nrow, 5)
+                                                                   )
+        conn.execute(sql_command)
         continue
     else:
         print ID
@@ -47,13 +56,19 @@ for nrow in range(nrows):
         print sheet.cell_value(nrow, 3)
         print sheet.hyperlink_map.get((nrow, 4)).url_or_path
         print sheet.cell_value(nrow, 5)
-        insert_item_comand = 'INSERT INTO youpinhui(ID, 品类, 商品名称, 日常价, 双12价, 商品链接, 推荐理由) VALUES ' \
-                            '(%d, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\")' % (ID, sheet.cell_value(nrow, 0),
-                                                                                      sheet.cell_value(nrow, 1),
-                                                                                      sheet.cell_value(nrow, 2),
-                                                                                      sheet.cell_value(nrow, 3),
-                                                                                      sheet.hyperlink_map.get((nrow, 4)).url_or_path,
-                                                                                      sheet.cell_value(nrow, 5))
+        insert_item_comand = 'INSERT INTO youpinhui(ID, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\") VALUES ' \
+                             '(%d, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\")' % (sheet.cell_value(0, 0),
+                                                                                       sheet.cell_value(0, 1),
+                                                                                       sheet.cell_value(0, 2),
+                                                                                       sheet.cell_value(0, 3),
+                                                                                       sheet.cell_value(0, 4),
+                                                                                       sheet.cell_value(0, 5),
+                                                                                       ID, sheet.cell_value(nrow, 0),
+                                                                                       sheet.cell_value(nrow, 1),
+                                                                                       sheet.cell_value(nrow, 2),
+                                                                                       sheet.cell_value(nrow, 3),
+                                                                                       sheet.hyperlink_map.get((nrow, 4)).url_or_path,
+                                                                                       sheet.cell_value(nrow, 5))
         conn.execute(insert_item_comand)
         conn.commit()
         ID += 1
